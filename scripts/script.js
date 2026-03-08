92416ac857459d4eaf9de2480ac4d28403534fda
Franciszek <FCzajkowski@proton.me>
2026-03-08 23:15:18 +0100

    Update button redirect URLs in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.getDatabase');
     
     buttons[0].addEventListener('click', function() {
-        window.location.href = 'https://github.com/FCzajkowski/RDSLite/tree/main';
+        window.location.href = 'https://github.com/FCzajkowski/RDSLite/releases/tag/official';
     });
     
     buttons[1].addEventListener('click', function() {
         window.location.href = 'https://github.com/FCzajkowski/RDSLite/blob/main/DOCUMENTATION.md';
     });
 });
+
